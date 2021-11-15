787712e5c305b4b2845da923b28e9a8034c9cf65
ytrms <lorenzo.gravina@me.com>
2021-11-15 22:37:39 +0300

    switch to cards from list

--- script.js
+++ script.js
@@ -23,9 +23,10 @@ class Book {
 
 const got = new Book("A Game of Thrones", "George Martin", 800, true);
 const lotr = new Book("The Fellowship of the Ring", "JRR Tolkien", 1000, true);
-let myLibrary = [got, lotr];
+const hobbit = new Book("The Hobbit", "JRR Tolkien", 300, true);
+let myLibrary = [got, lotr, hobbit];
 
-const bookTable = document.getElementsByClassName('bookList')[0];
+const cards = document.getElementsByClassName('bookList')[0];
 
 function addBookToDB() {
     const title = prompt("Title of the book");
@@ -33,22 +34,61 @@ function addBookToDB() {
     const pages = prompt("Number of pages in the book");
     const read = prompt("Have you read the book? true/false");
     myLibrary.push(new Book(title, author, Number(pages), Boolean(read)));
-    refreshBookList();
+}
+
+/**
+ * Gets a Book object and returns a formatted card
+ * for the book.
+ * @param {Book} book 
+ */
+function getBookCard(book) {
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    let cardTitle = document.createElement('div');
+    cardTitle.classList.add('cardTitle');
+    cardTitle.textContent = book.name;
+
+    let cardAuthor = document.createElement('div');
+    cardAuthor.classList.add('cardAuthor');
+    cardAuthor.textContent = book.author;
+
+    let cardPages = document.createElement('div');
+    cardPages.classList.add('cardPages');
+    cardPages.textContent = String(book.pages);
+
+    let cardReadStatus = document.createElement('div');
+    cardReadStatus.classList.add('cardReadStatus');
+    if (book.read) {
+        cardReadStatus.textContent = "Read";
+        cardReadStatus.classList.add('read');
+    } else {
+        cardReadStatus.textContent = "Not read yet";
+        cardReadStatus.classList.add('unread');
+    }
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPages);
+    card.appendChild(cardReadStatus);
+    return card;
 }
 
 function refreshBookList() {
-    bookTable.innerHTML = "";
+    cards.innerHTML = "";
     for (const key in myLibrary) {
         if (Object.hasOwnProperty.call(myLibrary, key)) {
             const book = myLibrary[key];
-            let bookCard = document.createElement('div');
-            bookCard.classList.toggle('card');
-            bookCard.textContent = book.info();
-            bookTable.appendChild(bookCard);
+            cards.appendChild(getBookCard(book));
         }
     }
+    console.log({ myLibrary });
 }
 
 const addBookButton = document.getElementsByClassName('addBook')[0];
-addBookButton.addEventListener('click', addBookToDB);
+addBookButton.addEventListener('click', () => {
+    addBookToDB();
+    refreshBookList();
+});
+
 refreshBookList();
